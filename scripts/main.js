3b94f7ffabe6bcf8e00d27006b395a01a84aa5fc
Amanbek <amanbekazizuulu@gmail.com>
2025-01-29 14:40:56 +0600

    split the code into several functions

--- scripts/main.js
+++ scripts/main.js
@@ -5,13 +5,20 @@ console.table(listOfUsers);
 
 // создание элементов
 const
-	$app = document.getElementById("app");
+	$app = document.getElementById("app"),
 	$addNewUser__form = document.getElementById("add-new-user-form");
 
 const
 	$table = document.createElement("table"),
 	$tableHead = document.createElement("thead"),
 	$tableBody = document.createElement("tbody");
+
+const
+	$firstName__input = document.getElementById("first-name-input"),
+	$middleName__input = document.getElementById("middle-name-input"),
+	$lastName__input = document.getElementById("last-name-input"),
+	$age__input = document.getElementById("age-input"),
+	$hobby__input = document.getElementById("hobby-input");
 
 const
 	$tableHeadTr = document.createElement("tr"),
@@ -23,7 +30,7 @@ const
 
 $table.classList.add("table", "table-striped-columns");
 
-$tableHeadTh__first_middle_last_names.textContent = "ФИО";
+$tableHeadTh__first_middle_last_names.textContent = "ФАМИЛИЯ ИМЯ ОТЧЕСТВО";
 $tableHeadTh__age.textContent = "Возраст"
 $tableHeadTh__year_of_birth.textContent = "Год рождения";
 $tableHeadTh__hobby.textContent = "Хобби";
@@ -40,26 +47,17 @@ $table.append($tableHead);
 $table.append($tableBody);
 $app.append($table);
 
-// рендер
-
-// 1. подготовка
-
-// 2. отрисовка
-
-let listOfUsers__copy = [...listOfUsers];																			// → создаю копию массива данных
-
-for (let user of listOfUsers__copy) {
+function createUser__table_row(user) {
 	const
 		$userTr = document.createElement("tr"),
-
 		$userTh__first_middle_last_names = document.createElement("th"),
 		$userTh__age = document.createElement("th"),
 		$userTh__year_of_birth = document.createElement("th"),
 		$userTh__hobby = document.createElement("th");
 
-	$userTh__first_middle_last_names.textContent = `${user.firstName} ` + `${user.middleName} ` + `${user.lastName} `;
+	$userTh__first_middle_last_names.textContent = `${user.fullName}`;
 	$userTh__age.textContent = `${user.age}`;
-	$userTh__year_of_birth.textContent = `${new Date().getFullYear() - user.age}`;
+	$userTh__year_of_birth.textContent = `${user.yearOfBirth}`;
 	$userTh__hobby.textContent = `${user.hobby}`;
 
 	$userTr.append($userTh__first_middle_last_names);
@@ -67,7 +65,53 @@ for (let user of listOfUsers__copy) {
 	$userTr.append($userTh__year_of_birth);
 	$userTr.append($userTh__hobby);
 
-	$tableBody.append($userTr);
+	return $userTr;
 }
 
+// рендер
 
+// 1. подготовка
+
+// 2. отрисовка
+
+function createUser() {
+	return {
+		firstName: $firstName__input.value,
+		middleName: $middleName__input.value,
+		lastName: $lastName__input.value,
+		age: parseInt($age__input.value),
+		hobby: $hobby__input.value
+	};
+}
+
+function renderListOfUsers(listToRender) {
+	$tableBody.innerHTML = '';
+
+	const listOfUsers__copy = [...listToRender];
+	
+	for (const user of listOfUsers__copy) {
+		user.fullName = `${user.lastName} ` + `${user.firstName} ` + `${user.middleName} `.trim();
+		user.yearOfBirth = `${new Date().getFullYear() - user.age}`;
+	}
+	for (const user of listOfUsers__copy) {
+		const $user__table_row_holder = createUser__table_row(user);
+
+		$tableBody.append($user__table_row_holder);
+	}
+};
+
+// добавление нового пользователя
+renderListOfUsers(listOfUsers);
+
+$addNewUser__form.addEventListener("submit", event => {
+	event.preventDefault();
+
+	listOfUsers.push(createUser());
+
+	renderListOfUsers(listOfUsers);
+
+	console.table(listOfUsers);
+
+	$addNewUser__form.reset();
+
+});
